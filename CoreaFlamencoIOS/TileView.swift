3e4a5d37baf311b5bfc4b1430765228fb705c5eb
songyungo <songyungo@gmail.com>
2019-05-13 17:36:03 +0900

    타일 Swift 파일 추가

---
+++ CoreaFlamencoIOS/TileView.swift
@@ -0,0 +1,17 @@
+//
+//  TileView.swift
+//  CoreaFlamencoIOS
+//
+//  Created by Yun Song on 13/05/2019.
+//  Copyright © 2019 Yun Song. All rights reserved.
+//
+
+import UIKit
+
+class TileView: UIView {
+    
+    @IBOutlet weak var img: UIImageView!
+    override func awakeFromNib() {
+        
+    }
+}
